8546341b84cbcbf699bba73681f43ca41e9acb53
O-Cui <mrowencui@gmail.com>
2022-10-24 23:25:39 -0700

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personal_portfolio';
+  nameInput='';
+  emailInput='';
+  url="https://formspree.io/f/mgeqnokw";
+  messageInput='';
+  title="Hello";
+
+  contactForm= new FormGroup({
+    name: new FormControl(this.nameInput, [
+      Validators.required
+    ]),
+    email: new FormControl(this.emailInput, [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    message: new FormControl(this.messageInput, [
+      Validators.required
+    ])
+
+  });
+
+  onSubmit(){
+    console.warn(this.contactForm.value);
+  }
+
+  get name() {
+    return this.contactForm.get('name')
+  }
+  get email() {
+    return this.contactForm.get('email')
+  }
+  get message() {
+    return this.contactForm.get('message')
+  }
 }
